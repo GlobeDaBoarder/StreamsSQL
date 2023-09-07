e176ba416788f8b6e7a43eb5dc5b9ad02b641819
GlobeDaBoarder <glebivashyn@gmail.com>
2023-09-07 16:57:59 +0300

    Solved Pavel's task

--- src/main/java/com/example/streamssql/Main.java
+++ src/main/java/com/example/streamssql/Main.java
@@ -58,30 +58,11 @@ public class Main {
                 new XmlFile("5", "ASCII", List.of(new Tag("sheet"), new Tag("column"), new Tag("row")))
         );
 
-        // Pavel's task:
-        // If XmlFile in it's list of tags has a "sheet" tagName count all tags of that XmlFile. If no tagName fo sheet, don't count it. Group by encoding .
-        //  So the result should be: {"UTF-8"=0, "ASCII"=5}, 5 because XmlFIle with ID 4 has 2 Tags, and one of them is sheet, so we count both. XmlFIle with ID 5 has 3 Tags, and one of them is sheet, so we count alll three of them. 3 + 2 = 5
-
-
-        Map<String, Long> encodingsToTagCounts = xmlFiles.stream()
-        .collect(Collectors.groupingBy(
-                        XmlFile::getEncoding,
-                        Collectors.filtering(
-                                xmlFile -> xmlFile.getTags().stream().map(Tag::getName).anyMatch(tagName -> tagName.equals("sheet")),
-                                Collectors.summingLong(xmlFile -> xmlFile.getTags().size())
-                        )
-                ));
-
-
-        System.out.println(encodingsToTagCounts);
 
 
         // Unique list of tags for each encoding type. Result should be: {UTF-8=[load, function, main, table], ASCII=[column, sheet, row]}
          Map<String, Set<String>> encodingTypeToUniqueTags = xmlFiles.stream()
-                 .collect(Collectors.groupingBy(
-                         XmlFile::getEncoding,
-                         Collectors.flatMapping(xmlFile -> xmlFile.getTags().stream().map(Tag::getName), Collectors.toSet())
-                 ));
+                         . ...
 
         System.out.println(encodingTypeToUniqueTags);
 
