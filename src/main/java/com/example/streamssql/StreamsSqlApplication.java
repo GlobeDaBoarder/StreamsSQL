18eeb2dd2af08cfe97437054f6900515bdc8e1ff
GlobeDaBoarder <glebivashyn@gmail.com>
2023-09-07 15:09:31 +0300

    Initial commit

---
+++ src/main/java/com/example/streamssql/StreamsSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamssql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamsSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/streamssql/StreamsSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamssql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
